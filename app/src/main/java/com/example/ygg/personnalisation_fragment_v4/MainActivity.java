20bc91ea4649419b42e1a2ea06a0d682c60b4127
Ygg <dans.la.com@gmail.com>
2017-10-20 22:59:54 +0200

    change color of Views

--- app/src/main/java/com/example/ygg/personnalisation_fragment_v4/MainActivity.java
+++ app/src/main/java/com/example/ygg/personnalisation_fragment_v4/MainActivity.java
@@ -8,12 +8,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.madrapps.pikolo.HSLColorPicker;
 import com.madrapps.pikolo.listeners.SimpleColorSelectionListener;
 
 import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,36 +29,37 @@ public class MainActivity extends AppCompatActivity {
         colorPicker.setColorSelectionListener(new SimpleColorSelectionListener() {
             @Override
             public void onColorSelected(int color) {
-                // Do whatever you want with the color
-//                ((View)findViewById(R.id.bgMain)).getBackground().setColorFilter(color, PorterDuff.Mode.MULTIPLY);
 
-                getSupportActionBar().setBackgroundDrawable( new ColorDrawable( color ));
-                View bg = (View) findViewById(R.id.mainBG);
+                getSupportActionBar().setBackgroundDrawable( new ColorDrawable( getContrastColor(color) ));
+                View bg = findViewById(R.id.mainBG);
 
-//                bg.setBackgroundColor( getContrastColor(color) );
-//                bg.setBackgroundColor( 0xFFAA22 );
-
-                TextView test_1 = (TextView) findViewById(R.id.test_1);
-                TextView test_2 = (TextView) findViewById(R.id.test_2);
-
-                test_1.setText( Integer.toString( color ) );
-                test_2.setText( Integer.toString( getContrastColor(color) ) );
-
-//                Log.d("Mes_test", Integer.toString(getContrastColor(color)) );
+                bg.setBackgroundColor( color );
 
 
-//                float r = 128 + ( 128 - Color.red(color) );
-//                float g = 128 + ( 128 - Color.green(color) );
-//                float b = 128 + ( 128 - Color.blue(color) );
+                findViewById(R.id.s1).setBackgroundColor(lighten(color, 0.8f));
+                findViewById(R.id.s2).setBackgroundColor(lighten(color, 0.8f));
+                findViewById(R.id.s3).setBackgroundColor(lighten(color, 0.8f));
 
-//                test_2.setText( Integer.toString( Color.rgb( (int)r, (int)g, (int)b ) ) );
-//                test_2.setText( Float.toString( r ) );
 
-//                txt.setTextColor(  );
-//                Color c = getContrastColor( Color.valueOf( color ) );
-////                Color c = getContrastColor( Color( color ) );
-//                Color c = Color.valueOf( color );
-//                txt.setText( c.toString() );
+
+//                // TEST ****
+//                int test = lighten(color, 0.8f);
+
+//                View test = findViewById(R.id.mainBG).findViewWithTag("mon_tag");
+//
+//                if(test != null) { test.setBackgroundColor(lighten(color, 0.8f)); }
+
+
+
+//                ArrayList<View> result = getViewsByTag( (ViewGroup) findViewById(R.id.mainBG), "mon_tag" );
+//
+//                if(!result.isEmpty()) {
+//                    View separate = result.get(0);
+//
+//                    if (separate != null) {
+//                        separate.setBackgroundColor(lighten(color, 0.8f));
+//                    }
+//                }
             }
         });
 
@@ -65,9 +68,42 @@ public class MainActivity extends AppCompatActivity {
     public static int getContrastColor(int color) {
 
         float r = 128 + ( 128 - Color.red(color) ),
-              g = 128 + ( 128 - Color.green(color) ),
-              b = 128 + ( 128 - Color.blue(color) );
+                g = 128 + ( 128 - Color.green(color) ),
+                b = 128 + ( 128 - Color.blue(color) );
 
         return Color.rgb( (int)r, (int)g, (int)b );
     }
+
+    public static int lighten(int color, float percent) {
+
+        float[] hsv = { 0f, 0f, 0f };
+        Color.colorToHSV(color, hsv);
+
+        Log.d("Mes_test", Float.toString( hsv[2] ));
+        Log.d("Mes_test", Integer.toString(Color.HSVToColor(hsv)));
+
+        hsv[2] *= percent;
+
+        return Color.HSVToColor(hsv);
+    }
+
+
+
+//    public static ArrayList<View> getViewsByTag(ViewGroup root, String tag){
+//        ArrayList<View> views = new ArrayList<View>();
+//        final int childCount = root.getChildCount();
+//        for (int i = 0; i < childCount; i++) {
+//            final View child = root.getChildAt(i);
+//            if (child instanceof ViewGroup) {
+//                views.addAll(getViewsByTag((ViewGroup) child, tag));
+//            }
+//
+//            final Object tagObj = child.getTag();
+//            if (tagObj != null && tagObj.equals(tag)) {
+//                views.add(child);
+//            }
+//
+//        }
+//        return views;
+//    }
 }
