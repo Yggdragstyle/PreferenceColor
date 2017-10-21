beb1ea3d7af2efa1cf3e39867f15921df2c6c8f9
Ygg <dans.la.com@gmail.com>
2017-10-21 17:23:44 +0200

    Color light & dark

--- app/src/main/java/com/example/ygg/personnalisation_fragment_v4/MainActivity.java
+++ app/src/main/java/com/example/ygg/personnalisation_fragment_v4/MainActivity.java
@@ -1,14 +1,21 @@
 package com.example.ygg.personnalisation_fragment_v4;
 
+import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.ColorDrawable;
 import android.support.constraint.ConstraintLayout;
+import android.support.v4.graphics.drawable.DrawableCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatCheckBox;
+import android.support.v7.widget.SwitchCompat;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 import com.madrapps.pikolo.HSLColorPicker;
@@ -30,15 +37,98 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onColorSelected(int color) {
 
-                getSupportActionBar().setBackgroundDrawable( new ColorDrawable( getContrastColor(color) ));
-                View bg = findViewById(R.id.mainBG);
 
+                SeekBar sb = (SeekBar) findViewById(R.id.seekbar);
+                Toolbar actionBarToolbar = (Toolbar)findViewById(R.id.action_bar);
+                View bg = findViewById(R.id.mainBG);
+                SwitchCompat switchCompat = (SwitchCompat) findViewById(R.id.switchControl);
+
+
+                int[][] states = new int[][] {
+                        new int[] {-android.R.attr.state_checked},
+                        new int[] {android.R.attr.state_checked},
+                };
+
+                int oposite = getContrastColor(color);
+
+
+
+                // ******************   T H E M E S   ****************** //
+
+                // ***** DARK *****
+
+                    int[] thumbColors = new int[] {
+                            Color.WHITE,
+                            lighten(oposite, 0.6f)
+                    };
+
+                    int[] trackColors = new int[] {
+                            Color.parseColor("#C0C0C0"),
+                            oposite
+                    };
+
+                    int extrude = lighten(color, 0.6f);
+                    int exO = lighten(oposite, 0.4f);
+
+                    int txt = Color.WHITE;
+                    int title = darken(color, 0.4f);
+
+
+                // ***** LIGHT *****
+                if( Color.luminance(color) > 0.2f ) {
+
+                    thumbColors = new int[] {
+                            Color.BLACK,
+                            darken(color, 0.6f)
+                    };
+
+                    trackColors = new int[] {
+                            Color.GRAY,
+                            lighten(color, 0.6f)
+                    };
+
+                    extrude = darken(color, 0.8f);
+                    exO = darken(oposite, 0.6f);
+
+                    txt = darken(color, 0.4f);
+                    title = Color.WHITE;
+                }
+                // ****************** //
+
+                // ***************************************************** //
+
+
+
+                // BACKGROUND
+                getSupportActionBar().setBackgroundDrawable( new ColorDrawable( oposite ));
                 bg.setBackgroundColor( color );
 
+                // SEPARATE
+                findViewById(R.id.s1).setBackgroundColor(extrude);
+                findViewById(R.id.s2).setBackgroundColor(extrude);
+                findViewById(R.id.s3).setBackgroundColor(extrude);
 
-                findViewById(R.id.s1).setBackgroundColor(lighten(color, 0.8f));
-                findViewById(R.id.s2).setBackgroundColor(lighten(color, 0.8f));
-                findViewById(R.id.s3).setBackgroundColor(lighten(color, 0.8f));
+                // SEEKBAR
+                sb.getProgressDrawable().setColorFilter( exO, PorterDuff.Mode.MULTIPLY);
+                sb.getThumb().setColorFilter( oposite, PorterDuff.Mode.SRC_ATOP);
+
+                // SWITCH
+                DrawableCompat.setTintList(DrawableCompat.wrap(switchCompat.getThumbDrawable()), new ColorStateList(states, thumbColors));
+                DrawableCompat.setTintList(DrawableCompat.wrap(switchCompat.getTrackDrawable()), new ColorStateList(states, trackColors));
+
+                // EDIT TEXT
+                ((EditText)findViewById(R.id.editText)).setTextColor(color);
+
+                // TEXT COLOR
+                ((TextView)findViewById(R.id.label1)).setTextColor(txt);
+                ((TextView)findViewById(R.id.label2)).setTextColor(txt);
+                ((TextView)findViewById(R.id.label3)).setTextColor(txt);
+
+                if (actionBarToolbar != null) { actionBarToolbar.setTitleTextColor(title); }
+
+
+
+
 
 
 
@@ -74,15 +164,22 @@ public class MainActivity extends AppCompatActivity {
         return Color.rgb( (int)r, (int)g, (int)b );
     }
 
+    public static int darken(int color, float percent) {
+
+        float[] hsv = { 0f, 0f, 0f };
+        Color.colorToHSV(color, hsv);
+
+        hsv[2] *= percent;
+
+        return Color.HSVToColor(hsv);
+    }
+
     public static int lighten(int color, float percent) {
 
         float[] hsv = { 0f, 0f, 0f };
         Color.colorToHSV(color, hsv);
 
-        Log.d("Mes_test", Float.toString( hsv[2] ));
-        Log.d("Mes_test", Integer.toString(Color.HSVToColor(hsv)));
-
-        hsv[2] *= percent;
+        hsv[2] /= percent;
 
         return Color.HSVToColor(hsv);
     }
